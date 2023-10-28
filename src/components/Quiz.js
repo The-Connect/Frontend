854001487ea19f353de4143dcd564154542a1fe2
AngieCana <grilozangie@gmail.com>
2023-10-28 19:45:01 -0400

    updated some filler questions and set up the beginning logic for the quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,23 +1,41 @@
-import React from 'react'
-import { useState } from 'react';
-import"./Quiz.css"
+import React from "react";
+import { useState } from "react";
+import "./Quiz.css";
 
 export default function Quiz() {
+  //answerPicked will be set to false initially so that users cannot move on to the next question without selecting an answer. Once they have selected answer, answerPicked will be set to true
+  const [answerPicked, setAnswerPicked] = useState(false);
 
-    const Qs = [{
-        qText: '',
-        id: 0,
-        answersA:"",
-        answersB:"",
-        AnswersC:"",
-        AnswersD:"", 
-    }]
+  //will check where the user is in terms of questions
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    const [currentQ, setCurrentQ] = useState(0);
+  //holds arrays of questions 
+  const questions = [
+    {
+      questionText: "Are you an introvert or an extrovert?",
+      answerOptions: [
+        { answerText: "Introvert", answerValue: "Personality: introverted" },
+        { answerText: "Extrovert", answerValue: "Personality: extroverted" },
+      ],
+    },
+    {
+      questionText: "Do you like going out in nature or sticking to the city?",
+      answerOptions: [
+        {answerText: "Nature", answerValue: "nature"},
+        {answerText: "City", answerValue: "city"}
+      ]
+    }
+  ];
+
+  const [currentQ, setCurrentQ] = useState(0);
   return (
-    <div className='quiz-container'>
-        <h1>Take the quiz to find your connections</h1>
-        <fieldset></fieldset>
+    <div className="quiz-container">
+      <h1>Take the quiz to find your connections</h1>
+      <fieldset>
+        <div className="question-container">
+          <div className="question-counter"></div>
+        </div>
+      </fieldset>
     </div>
-  )
+  );
 }
