8a2c4b68fd36a15b1752dabcb22a428d29a9d31a
AngieCana <grilozangie@gmail.com>
2023-10-29 13:54:18 -0400

    working version of the quiz.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -14,8 +14,8 @@ export default function Quiz() {
     {
       questionText: "Are you an introvert or an extrovert?",
       answerOptions: [
-        { answerText: "Introvert", answerValue: "Personality: introverted" },
-        { answerText: "Extrovert", answerValue: "Personality: extroverted" },
+        { answerText: "Introvert", answerValue: "introverted" },
+        { answerText: "Extrovert", answerValue: "extroverted" },
       ],
     },
     {
@@ -27,15 +27,35 @@ export default function Quiz() {
     }
   ];
 
-  const [currentQ, setCurrentQ] = useState(0);
+  const handleButtonClicked = () => {
+    const nextQuestion = currentQuestion + 1;
+    if(nextQuestion < questions.length){
+      setCurrentQuestion(nextQuestion);
+      setQuestionNumber(questionNumber + 1)
+    } else {
+      alert('You have reached the end of this quiz.')
+    }
+    
+  }
+
+  const [questionNumber, setQuestionNumber] = useState(0);
+
   return (
     <div className="quiz-container">
       <h1>Take the quiz to find your connections</h1>
+      <>
       <fieldset>
         <div className="question-container">
-          <div className="question-counter"></div>
+          <div className="question-counter">
+            <span>Question {questionNumber + 1}/</span>{questions.length}
+            <div className="question-text">{questions[currentQuestion].questionText}</div>
+          </div>
+          <div className="answers">
+            {questions[currentQuestion].answerOptions.map((answerOption) => <button onClick={handleButtonClicked}>{answerOption.answerText}</button> )}
+          </div>
         </div>
       </fieldset>
+      </>
     </div>
   );
 }
