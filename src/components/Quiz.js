04b5a5670def79459c936585c661df8e8fd278f1
AngieCana <grilozangie@gmail.com>
2023-12-06 13:57:27 -0500

    newest commit

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 import "./Quiz.css";
 
 export default function Quiz() {
-  // answerPicked will be set to false initially so that users cannot move on to 
-  // the next question without selecting an answer. Once they have 
+  // answerPicked will be set to false initially so that users cannot move on to
+  // the next question without selecting an answer. Once they have
   // selected answer, answerPicked will be set to true
   const [answerPicked, setAnswerPicked] = useState(false);
+  const [userAnswers, setUserAnswers] = useState([]);
 
   //will check where the user is in terms of questions
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -20,91 +21,87 @@ export default function Quiz() {
         answerPicked: false,
         answerId: answerIds[0],
       },
-      { 
-        answerText: "3 - Agree", 
-        answerValue: 3, 
-        answerPicked: false, 
-        answerId: answerIds[1]
+      {
+        answerText: "3 - Agree",
+        answerValue: 3,
+        answerPicked: false,
+        answerId: answerIds[1],
       },
       {
         answerText: "2 - Slightly Agree",
         answerValue: 2,
         answerPicked: false,
-        answerId: answerIds[2]
-
+        answerId: answerIds[2],
       },
       {
         answerText: "1 - Disagree",
         answerValue: 1,
         answerPicked: false,
-        answerId: answerIds[3]
+        answerId: answerIds[3],
       },
       {
         answerText: "0 - Strongly Disagree",
         answerValue: 0,
         answerPicked: false,
-        answerId: answerIds[4]
+        answerId: answerIds[4],
       },
-    ]
-  })
-//   console.log(buildQuestion(
-//     "Core Diaries", 
-//   "I'm drawn to roles where I can take charge or lead initiatives."
-//   ,[1,2,3,4])
-// )
-  
-const questions = [
-  buildQuestion(
-    "Core Diaries",
-    "I'm drawn to roles where I can take charge or lead initiatives.",
-    [1, 2, 3, 4, 5]
-  ),
-  buildQuestion(
-    "Core Diaries",
-    "Meeting goals and targets at work gives me a feeling of accomplishment.",
-    [5, 6, 7, 8, 9]
-  ),
-  buildQuestion(
-    "Core Desires:",
-    "Building professional relationships is vital for my career growth.",
-    [10, 11, 12, 13,14]
-  ),
-  buildQuestion(
-    "Core Desires:",
-    "Recognition for my work, whether through feedback or rewards, matters a lot to me.",
-    [15, 16, 17, 18, 19]
-  ),
-  buildQuestion(
-    "Core Desires:",
-    "A clear job role with set responsibilities is something I prefer.",
-    [20, 21, 22, 23, 24]
-  ),
-  buildQuestion(
-    "Lifestyle Habits:",
-    "I'm open to roles that might involve unconventional hours or remote work.",
-    [25, 26, 27, 28, 29]
-  )
-];
+    ],
+  });
+  //   console.log(buildQuestion(
+  //     "Core Diaries",
+  //   "I'm drawn to roles where I can take charge or lead initiatives."
+  //   ,[1,2,3,4])
+  // )
+
+  const questions = [
+    buildQuestion(
+      "Core Diaries",
+      "I'm drawn to roles where I can take charge or lead initiatives.",
+      [1, 2, 3, 4, 5]
+    ),
+    buildQuestion(
+      "Core Diaries",
+      "Meeting goals and targets at work gives me a feeling of accomplishment.",
+      [5, 6, 7, 8, 9]
+    ),
+    buildQuestion(
+      "Core Desires:",
+      "Building professional relationships is vital for my career growth.",
+      [10, 11, 12, 13, 14]
+    ),
+    buildQuestion(
+      "Core Desires:",
+      "Recognition for my work, whether through feedback or rewards, matters a lot to me.",
+      [15, 16, 17, 18, 19]
+    ),
+    buildQuestion(
+      "Core Desires:",
+      "A clear job role with set responsibilities is something I prefer.",
+      [20, 21, 22, 23, 24]
+    ),
+    buildQuestion(
+      "Lifestyle Habits:",
+      "I'm open to roles that might involve unconventional hours or remote work.",
+      [25, 26, 27, 28, 29]
+    ),
+  ];
 
   //when a user clicks an answer. This should also trigger one answerPicked to become true
   const handleButtonClicked = (answerValue) => {
     const nextQuestion = currentQuestion + 1;
     const updatedQuestions = [...questions];
-    updatedQuestions[currentQuestion].answerOptions.forEach((option) => {
-      option.answerPicked = option.answerValue === answerValue;
-      //updatedQuestions variable is a shallow copy of the questions array
-      //updatedQuestions has currentQuestion passed through it and then accesses 
-      // the answerOption. For each option(parameter) 
-      // a function is passed: your answerpicked option is assigned to your 
-      // option.answerValue, and then it will check if your answerValue is 
-      // strictly equal to your option.answerValue
-    });
+    const selectedAnswer = updatedQuestions[currentQuestion].answerOptions.find(
+      (option) => option.answerValue === answerValue
+      //variable selectedAnswer is calling updatedQuestions(which it itself is a copy of the array of objects questions) and then passing through the current question and the answerOptions for the current question and then finding the answerValue and seeing if it is strictly equal to answerValue
+    );
+    setUserAnswers([...userAnswers, {question: currentQuestion, answer: answerValue}]);
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
       setQuestionNumber(questionNumber + 1);
       setAnswerPicked(true);
       console.log("Button Click received");
       console.log(questions);
+      console.log("user answers:", userAnswers)
     } else {
       alert("You have reached the end of this quiz.");
     }
@@ -124,7 +121,6 @@ const questions = [
   };
 
   const [questionNumber, setQuestionNumber] = useState(0);
-
 
   return (
     <div className="quiz-container">
@@ -173,6 +169,10 @@ const questions = [
                   {answerOption.answerText}
                 </button>
               ))}
+              {/* this should display the score of what the user picked  */}
+              {questions[currentQuestion].answerOptions.map(
+                (answerValue) => {}
+              )}
             </div>
             <div className="options-container">
               <button className="reset-button" onClick={resetButtonClicked}>
